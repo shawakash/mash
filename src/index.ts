ea718922f4430ba08e545cd73f6a7aaa2452be6e
Akash Shaw <shawakash2003@gmail.com>
2024-09-04 21:26:14 +0530

    chore: adds route for csrf token auth

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,63 @@ import fs from "fs/promises";
 import path from "path";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
+import crypto from "crypto";
 
 const app = express();
 const photosDir = path.join(__dirname, "../public/photos");
 const ratingsFile = path.join(__dirname, "../ratings.json");
-app.use(cors());
 
 const browserCheck = (req: Request, res: Response, next: NextFunction) => {
   const userAgent = req.headers["user-agent"];
   if (!userAgent || !userAgent.includes("Mozilla")) {
     return res.status(403).send("Access forbidden: Browser requests only");
+  }
+  next();
+};
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(browserCheck);
+
+app.get("/", (req: Request, res: Response) => {
+  //@ts-ignore
+  res.render("index", { csrfToken: req.csrfToken() });
+});
+
+const generateCSRFToken = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.cookies["XSRF-TOKEN"]) {
+    const csrfToken = crypto.randomBytes(64).toString("hex");
+    res.cookie("XSRF-TOKEN", csrfToken, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+  }
+  next();
+};
+
+app.use(generateCSRFToken);
+
+const csrfProtection = (req: Request, res: Response, next: NextFunction) => {
+  const csrfCookie = req.cookies["XSRF-TOKEN"];
+  const csrfHeader = req.headers["x-csrf-token"];
+
+  if (!csrfCookie || !csrfHeader || csrfCookie !== csrfHeader) {
+    return res.status(403).json({ error: "Invalid CSRF token" });
+  }
+
+  next();
+};
+
+const refererCheck = (req: Request, res: Response, next: NextFunction) => {
+  const referer = req.headers.referer;
+  if (!referer || !referer.includes(req.get("host")!)) {
+    return res.status(403).send("Access forbidden: Invalid referer");
   }
   next();
 };
@@ -23,9 +70,6 @@ const limiter = rateLimit({
 });
 
 // app.use(limiter);
-// app.use(browserCheck);
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.json());
 
 let globalRatings: { [key: string]: number } = {};
 
@@ -60,24 +104,37 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-app.get("/photos", async (req: Request, res: Response) => {
-  try {
-    const files = await fs.readdir(photosDir);
-    const photoFiles = files.filter((file) =>
-      /\.(jpg|jpeg|png|gif)$/.test(file),
-    );
-    res.json(photoFiles);
-  } catch (err) {
-    console.error(`Error reading directory ${photosDir}:`, err);
-    res.status(500).send("Unable to scan directory");
-  }
+app.get("/get-csrf-token", (req, res) => {
+  const csrfToken = crypto.randomBytes(64).toString("hex");
+  res.cookie("XSRF-TOKEN", csrfToken, {
+    httpOnly: false,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+  res.json({ csrfToken });
 });
 
-app.get("/initial-ratings", async (req, res) => {
+app.get(
+  "/photos",
+  refererCheck,
+  csrfProtection,
+  async (req: Request, res: Response) => {
+    try {
+      const files = await fs.readdir(photosDir);
+      const photoFiles = files.filter((file) =>
+        /\.(jpg|jpeg|png|gif)$/.test(file),
+      );
+      res.json(photoFiles);
+    } catch (err) {
+      console.error(`Error reading directory ${photosDir}:`, err);
+      res.status(500).send("Unable to scan directory");
+    }
+  },
+);
+
+app.get("/initial-ratings", refererCheck, csrfProtection, async (req, res) => {
   try {
-    const data = await fs.readFile(ratingsFile, "utf8");
-    const ratings = JSON.parse(data);
-    res.json(ratings);
+    res.json(globalRatings);
   } catch (error: any) {
     if (error.code === "ENOENT") {
       res.json({});
@@ -88,31 +145,41 @@ app.get("/initial-ratings", async (req, res) => {
   }
 });
 
-app.post("/update-ratings", async (req: Request, res: Response) => {
-  const { ratings } = req.body;
-  if (!ratings || typeof ratings !== "object") {
-    return res.status(400).send("Invalid input");
-  }
-
-  for (const [photo, newRating] of Object.entries(
-    ratings as { [key: string]: number },
-  )) {
-    if (typeof newRating === "number") {
-      globalRatings[photo] = newRating;
+app.post(
+  "/update-ratings",
+  refererCheck,
+  csrfProtection,
+  async (req: Request, res: Response) => {
+    const { ratings } = req.body;
+    if (!ratings || typeof ratings !== "object") {
+      return res.status(400).send("Invalid input");
     }
-  }
 
-  await saveRatings();
-  res.status(200).send("Ratings updated");
-});
+    for (const [photo, newRating] of Object.entries(
+      ratings as { [key: string]: number },
+    )) {
+      if (typeof newRating === "number") {
+        globalRatings[photo] = newRating;
+      }
+    }
 
-app.get("/summary", (req: Request, res: Response) => {
-  const sortedRatings = Object.entries(globalRatings)
-    .sort(([, a], [, b]) => b - a)
-    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+    await saveRatings();
+    res.status(200).send("Ratings updated");
+  },
+);
 
-  res.json(sortedRatings);
-});
+app.get(
+  "/summary",
+  refererCheck,
+  csrfProtection,
+  (req: Request, res: Response) => {
+    const sortedRatings = Object.entries(globalRatings)
+      .sort(([, a], [, b]) => b - a)
+      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+    res.json(sortedRatings);
+  },
+);
 
 const port = 3000;
 app.listen(port, () => {
