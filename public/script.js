5d55cf8bfce168a096c965bd89b4009488b1552a
Akash Shaw <shawakash2003@gmail.com>
2024-09-04 19:48:16 +0530

    chore: script file

---
+++ public/script.js
@@ -0,0 +1,169 @@
+const photos = [];
+let ratings = {};
+let displayedPhotos = new Set();
+let allPhotosRated = false;
+let lastClickedPhoto = null;
+
+Promise.all([
+  fetch("/photos").then((response) => response.json()),
+  fetch("/initial-ratings").then((response) => response.json()),
+])
+  .then(([photoData, ratingData]) => {
+    photos.push(...photoData);
+    ratings = ratingData;
+
+    photos.forEach((photo) => {
+      if (!(photo in ratings)) {
+        ratings[photo] = 1000;
+      }
+    });
+
+    init();
+  })
+  .catch((error) => {
+    console.error("Error fetching initial data:", error);
+  });
+
+function getTopPhoto(excludePhoto) {
+  const sortedPhotos = Object.entries(ratings)
+    .filter(([photo]) => !displayedPhotos.has(photo) && photo !== excludePhoto)
+    .sort((a, b) => b[1] - a[1]);
+
+  if (sortedPhotos.length === 0) {
+    if (lastClickedPhoto) {
+      showWinner();
+      return null;
+    } else {
+      displayedPhotos.clear();
+      return getTopPhoto(excludePhoto);
+    }
+  }
+
+  const [topPhoto] = sortedPhotos[0];
+  displayedPhotos.add(topPhoto);
+
+  return topPhoto;
+}
+
+function sendRatingsToServer() {
+  fetch("/update-ratings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ratings }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to update ratings");
+      }
+      return response.text();
+    })
+    .then((result) => console.log(result))
+    .catch((error) => console.error("Error:", error));
+}
+
+function showWinner() {
+  const winnerPhoto = lastClickedPhoto;
+  const photoContainer = document.querySelector(".photo-container");
+  const winnerPhotoElement = document.getElementById("winnerPhoto");
+  const messageElement = document.getElementById("message");
+  const refreshButton = document.getElementById("refreshButton");
+
+  photoContainer.style.display = "none";
+  winnerPhotoElement.src = `./photos/${winnerPhoto}`;
+  winnerPhotoElement.style.display = "block";
+  messageElement.textContent =
+    "No more photos to rate. This is the winning photo!";
+  messageElement.style.display = "block";
+  refreshButton.style.display = "block";
+
+  winnerPhotoElement.animate(
+    [
+      { opacity: 0, transform: "scale(0.9)" },
+      { opacity: 1, transform: "scale(1)" },
+    ],
+    {
+      duration: 500,
+      easing: "ease-out",
+    },
+  );
+
+  allPhotosRated = true;
+
+  fetch("/summary")
+    .then((response) => response.json())
+    .then((summary) => {
+      console.log("Final Ratings:", summary);
+    });
+}
+
+window.addEventListener("close", (event) => {
+  if (!allPhotosRated) {
+    sendRatingsToServer();
+  }
+});
+
+function updateRatings(winner, loser) {
+  const k = 32;
+  const ratingWinner = ratings[winner];
+  const ratingLoser = ratings[loser];
+
+  const expectedScoreWinner =
+    1 / (1 + Math.pow(10, (ratingLoser - ratingWinner) / 400));
+
+  ratings[winner] = ratingWinner + k * (1 - expectedScoreWinner);
+  // ratings[loser] = ratingLoser + k * (expectedScoreWinner - 1);
+}
+
+const init = () => {
+  const photo1Element = document.getElementById("photo1");
+  const photo2Element = document.getElementById("photo2");
+
+  if (!photo1Element || !photo2Element) {
+    console.error("Photo elements not found.");
+    return;
+  }
+
+  updatePhotos();
+
+  photo1Element.onclick = () => {
+    const winner = photo1Element.src.split("/").pop();
+    const loser = photo2Element.src.split("/").pop();
+    lastClickedPhoto = winner;
+    updateRatings(winner, loser);
+    updatePhoto(photo2Element, winner);
+  };
+
+  photo2Element.onclick = () => {
+    const winner = photo2Element.src.split("/").pop();
+    const loser = photo1Element.src.split("/").pop();
+    lastClickedPhoto = winner;
+    updateRatings(winner, loser);
+    updatePhoto(photo1Element, winner);
+  };
+};
+
+function updatePhotos() {
+  const photo1 = getTopPhoto();
+  const photo2 = getTopPhoto(photo1);
+
+  if (!photo1 || !photo2) return;
+
+  const photo1Element = document.getElementById("photo1");
+  const photo2Element = document.getElementById("photo2");
+
+  photo1Element.src = `./photos/${photo1}`;
+  photo2Element.src = `./photos/${photo2}`;
+}
+
+function updatePhoto(photoElement, winnerPhoto) {
+  const newPhoto = getTopPhoto(winnerPhoto);
+
+  if (!newPhoto) {
+    sendRatingsToServer();
+    showWinner();
+    return;
+  }
+  photoElement.src = `./photos/${newPhoto}`;
+}
